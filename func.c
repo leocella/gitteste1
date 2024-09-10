f20f1518d8b44edbfe2ba232a670f01dfd3c8a18
Leonardo Cella <leeoceella@gmail.com>
2024-09-10 14:23:15 -0300

    adcionando funcao de multiplicacao

--- func.c
+++ func.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int soma(int a, int b) {
-    return a + b;
+int mult(int a, int b) {
+    return a*b;
 }
 
 int main() {
-    int resultado = soma(3, 4);
-    printf("A soma é: %d\n", resultado);
+    int mult = soma(3, 4);
+    printf("A soma é: %d\n", mult);
     return 0;
 }
