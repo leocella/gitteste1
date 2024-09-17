cabbe46fb058be3260eb71c15b10543c292a954c
Otnael Rocha <otnael@gmail.com>
2024-09-17 01:00:11 -0300

    Adicionado comentários da finalidade da função e espaçamento no código

--- func.c
+++ func.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int mult(int a, int b) {
+    /* Multiplica a por b*/
     return a*b;
 }
 
+
 int main() {
+    /* método principal do prog */
+    
     int mult = soma(3, 4);
+    
     printf("A soma é: %d\n", mult);
+    
     return 0;
 }
